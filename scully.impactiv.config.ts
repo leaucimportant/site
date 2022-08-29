1dae93c01814c51e8ae8fcf2a2645ffc774eea91
Elias <e.ghazali@impactiv.fr>
2022-08-29 09:54:53 +0200

    fix: undefined project

--- scully.impactiv.config.ts
+++ scully.impactiv.config.ts
@@ -1,6 +1,7 @@
 import { ScullyConfig } from '@scullyio/scully';
-import '@scullyio/scully-plugin-puppeteer';
+
 /** this loads the default render plugin, remove when switching to something else. */
+import '@scullyio/scully-plugin-puppeteer';
 
 export const config: ScullyConfig = {
   projectRoot: './src',
