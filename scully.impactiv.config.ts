88b853187b5ee43d660ccb1ab7b21b0f152dd360
Bastien Dufaud <b.dufaud@impactiv.fr>
2022-08-31 10:53:45 +0200

    build: changement paramètres build Angular & Scully
    Préparation pour Gitlab Pages

--- scully.impactiv.config.ts
+++ scully.impactiv.config.ts
@@ -6,6 +6,6 @@ import '@scullyio/scully-plugin-puppeteer';
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'impactiv',
-  outDir: './dist/static',
+  outDir: './public',
   routes: {},
 };
