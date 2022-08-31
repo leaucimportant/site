ce3dfe5d8f8c5632b2b06fc7548b168f430fb78e
Elias <e.ghazali@impactiv.fr>
2022-08-31 14:37:34 +0200

    feat: postRender email example

--- scully.impactiv.config.ts
+++ scully.impactiv.config.ts
@@ -1,13 +1,25 @@
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig, setPluginConfig } from '@scullyio/scully';
+import { getRegexPlugin } from '@gammastream/scully-plugin-regex';
 
 /** this loads the default render plugin, remove when switching to something else. */
 import '@scullyio/scully-plugin-puppeteer';
+
+const RegexPlugin = getRegexPlugin();
+setPluginConfig(RegexPlugin, {
+  replacements: [
+    {
+      from: 'href="mailto:test@example.fr"',
+      to: '',
+    },
+  ],
+});
 
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'impactiv',
   outDir: './public',
   routes: {},
+  defaultPostRenderers: [RegexPlugin],
   puppeteerLaunchOptions: {
     args: ['--no-sandbox', '--disable-setuid--sandbox'],
   },
