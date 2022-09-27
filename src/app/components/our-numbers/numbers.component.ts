943c7265bd5b3817c4e49fb2b95e3d211d1106bb
Amélie Laurent <a.laurent@impactiv.fr>
2022-09-27 14:29:53 +0200

    feat: add numbers component

---
+++ src/app/components/our-numbers/numbers.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'impactiv-numbers',
+  templateUrl: './numbers.component.html',
+  styleUrls: ['./numbers.component.scss'],
+})
+export class numbersComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
