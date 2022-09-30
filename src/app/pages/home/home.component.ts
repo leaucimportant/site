a626888fd3742f971842070a025ba894de53e526
Amélie LAURENT <a.laurent@impactiv.fr>
2022-09-30 10:06:12 +0000

    Resolve "quote component"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,7 +21,8 @@ export class HomeComponent implements OnInit {
     {
       data: '10 ans',
       title: 'D’expérience',
-      description: 'Écoute, agilité et satisfaction, sont au coeur de nos préoccupations.',
+      description:
+        'Écoute, agilité et satisfaction, sont au coeur de nos préoccupations.',
     },
     {
       data: '6K',
@@ -29,6 +30,18 @@ export class HomeComponent implements OnInit {
       description: 'Un parc d’affichage dynamique monitoré en temps-réel !',
     },
   ];
+
+  quote = {
+    description: 'de satisfaction client',
+    name: 'Steve Attal',
+    number: '100%',
+    job: 'Responsable SI, Groupe Elior',
+    avatar: 'assets/images/contents/quote/avatar_attal.png',
+    logo: 'assets/images/contents/quote/elior.png',
+    alt: 'Logo Elior',
+    content:
+      'Le groupe Elior cherche toujours des solutions digitales innovantes et rapides à mettre en œuvre. C’est une des raisons essentielles qui nous a fait choisir Impactiv. Nous avons toujours apprécié l’agilité et la rapidité d\'exécution dont ils font preuve sur tous nos projets. Mais ce que j’apprécie le plus reste leur professionnalisme.',
+  };
 
   constructor() {}
 
