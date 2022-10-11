c6a67c64cc47d2641fa1922ea90f7cac81e69cb3
Elias GHAZALI <e.ghazali@impactiv.fr>
2022-10-11 08:07:06 +0000

    Resolve "Solution DSM - Our expertise"

--- src/app/components/solutions/our-expertise/our-expertise.component.ts
+++ src/app/components/solutions/our-expertise/our-expertise.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgxGlideComponent } from 'ngx-glide';
 
 @Component({
   selector: 'impactiv-our-expertise',
   templateUrl: './our-expertise.component.html',
-  styleUrls: ['./our-expertise.component.scss']
+  styleUrls: ['./our-expertise.component.scss'],
 })
 export class OurExpertiseComponent implements OnInit {
+  @ViewChild('ngxGlide') ngxGlide!: NgxGlideComponent;
 
-  constructor() { }
+  expertises = [
+    {
+      title: 'La conception de l’expérience utilisateur. 1',
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
+      image: 'affluence.png',
+      alt: 'alt',
+    },
+    {
+      title: 'Designer avec un objectif en tête. 2',
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
 
-  ngOnInit(): void {
+      image: 'affluence.png',
+      alt: 'alt',
+    },
+    {
+      title: 'Designer avec un objectif en tête. 3',
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
+
+      image: 'affluence.png',
+      alt: 'alt',
+    },
+    {
+      title: 'Designer avec un objectif en tête. 4',
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
+
+      image: 'affluence.png',
+      alt: 'alt',
+    },
+    {
+      title: 'Designer avec un objectif en tête. 5',
+      items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'],
+
+      image: 'affluence.png',
+      alt: 'alt',
+    },
+  ];
+
+  currentStep = 0;
+
+  ngOnInit(): void {}
+
+  selectPage(isNext: boolean): void {
+    this.ngxGlide.go(isNext ? '>' : '<');
   }
 
+  onRanAfter(): void {
+    this.currentStep = this.ngxGlide.getIndex();
+  }
 }
