c447eaf1dfcd0a6fe50e4287ad47d97f856ac362
Amélie LAURENT <a.laurent@impactiv.fr>
2022-10-17 11:51:11 +0000

    Resolve "Quote Component"

--- src/app/components/usecase/use-case-quote/use-case-quote.component.ts
+++ src/app/components/usecase/use-case-quote/use-case-quote.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Block } from 'src/app/interfaces';
 
 @Component({
   selector: 'impactiv-use-case-quote',
   templateUrl: './use-case-quote.component.html',
-  styleUrls: ['./use-case-quote.component.scss']
+  styleUrls: ['./use-case-quote.component.scss'],
 })
 export class UseCaseQuoteComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() quote!: Block;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
