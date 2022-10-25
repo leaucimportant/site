5cd1a2aa5304f3f459f1d14ca14c58402557a0e7
Elias GHAZALI <e.ghazali@impactiv.fr>
2022-10-25 11:17:02 +0200

    feat: customers list

--- src/app/components/home/our-customers/our-customers.component.ts
+++ src/app/components/home/our-customers/our-customers.component.ts
@@ -8,24 +8,60 @@ import { Component } from '@angular/core';
 export class OurCustomersComponent {
   customers = [
     {
-      url: 'assets/images/contents/customers/logoipsum.svg',
-      alt: 'ipsum',
+      url: 'assets/images/contents/customers/ca.png',
+      alt: 'Crédit Agricole',
     },
     {
-      url: 'assets/images/contents/customers/logoipsum.svg',
-      alt: 'ipsum',
+      url: 'assets/images/contents/customers/cbre.png',
+      alt: 'CBRE',
     },
     {
-      url: 'assets/images/contents/customers/logoipsum.svg',
-      alt: 'ipsum',
+      url: 'assets/images/contents/customers/cea.png',
+      alt: 'CEA',
     },
     {
-      url: 'assets/images/contents/customers/logoipsum.svg',
-      alt: 'ipsum',
+      url: 'assets/images/contents/customers/constructa.png',
+      alt: 'Constructa',
     },
     {
-      url: 'assets/images/contents/customers/logoipsum.svg',
-      alt: 'ipsum',
+      url: 'assets/images/contents/customers/dupont.png',
+      alt: 'Dupont Restauration',
+    },
+    {
+      url: 'assets/images/contents/customers/elior.png',
+      alt: 'Elior',
+    },
+    {
+      url: 'assets/images/contents/customers/inrae.png',
+      alt: 'Inrae',
+    },
+    {
+      url: 'assets/images/contents/customers/la_poste.png',
+      alt: 'La poste',
+    },
+    {
+      url: 'assets/images/contents/customers/loreal.png',
+      alt: "L'Oreal",
+    },
+    {
+      url: 'assets/images/contents/customers/mcdo.png',
+      alt: "McDonald's",
+    },
+    {
+      url: 'assets/images/contents/customers/petitbateau.png',
+      alt: 'Petit bateau',
+    },
+    {
+      url: 'assets/images/contents/customers/relais_dor.png',
+      alt: "Relai d'or",
+    },
+    {
+      url: 'assets/images/contents/customers/sodexo.png',
+      alt: 'Sodexo',
+    },
+    {
+      url: 'assets/images/contents/customers/sport2000.png',
+      alt: 'Sport 2000',
     },
   ];
 }
