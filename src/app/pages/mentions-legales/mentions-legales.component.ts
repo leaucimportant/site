f1a18bf19becc9455897386df60d4f5b50e436b0
Elias GHAZALI <e.ghazali@impactiv.fr>
2022-11-07 09:14:46 +0000

    Resolve "Page Mentions légales"

---
+++ src/app/pages/mentions-legales/mentions-legales.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'impactiv-mentions-legales',
+  templateUrl: './mentions-legales.component.html',
+  styleUrls: ['./mentions-legales.component.scss'],
+})
+export class MentionsLegalesComponent {}
