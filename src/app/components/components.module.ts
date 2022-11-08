be0eb09b65bf54f4123a959da776f177eef113af
Elias GHAZALI <e.ghazali@impactiv.fr>
2022-11-08 15:57:28 +0000

    Resolve "partners component"

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,33 +4,35 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgxGlideModule } from 'ngx-glide';
 
+import { CompanyHeroComponent } from './company/company-hero/company-hero.component';
+import { OurPartnersComponent } from './company/our-partners/our-partners.component';
+import { TimelineComponent } from './company/timeline/timeline.component';
+import { BannerComponent } from './core/banner/banner.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { HeaderComponent } from './core/header/header.component';
 import { NumbersComponent } from './core/numbers/numbers.component';
-import { BannerComponent } from './core/banner/banner.component';
-import { OurCustomersComponent } from './home/our-customers/our-customers.component';
-import { OurSolutionsComponent } from './home/our-solutions/our-solutions.component';
-import { OurSkillsComponent } from './home/our-skills/our-skills.component';
-import { QuoteComponent } from './core/quote/quote.component';
-import { WebAppFeaturesComponent } from './solutions/web-app-features/web-app-features.component';
-import { HomeHeroComponent } from './home/home-hero/home-hero.component';
+import { OurCustomersComponent } from './core/our-customers/our-customers.component';
 import { StatsNumbersComponent } from './core/stats-numbers/stats-numbers.component';
-import { SolutionWebAppHeroComponent } from './solutions/web-app-hero/web-app-hero.component';
-import { SolutionCarouselComponent } from './solutions/solution-carousel/solution-carousel.component';
-import { SolutionBlockComponent } from './solutions/solution-block/solution-block.component';
-import { DsmHeroComponent } from './solutions/dsm-hero/dsm-hero.component';
-import { OurExpertiseComponent } from './solutions/our-expertise/our-expertise.component';
-import { InfrastructureMasonryComponent } from './solutions/infrastructure-masonry/infrastructure-masonry.component';
+import { TeamQuoteComponent } from './core/team-quote/team-quote.component';
 import { UseCaseResumeComponent } from './core/use-case-resume/use-case-resume.component';
+import { HomeHeroComponent } from './home/home-hero/home-hero.component';
+import { OurSkillsComponent } from './home/our-skills/our-skills.component';
+import { OurSolutionsComponent } from './home/our-solutions/our-solutions.component';
+import { DsmHeroComponent } from './solutions/dsm-hero/dsm-hero.component';
+import { InfrastructureMasonryComponent } from './solutions/infrastructure-masonry/infrastructure-masonry.component';
 import { OtherSolutionsComponent } from './solutions/other-solutions/other-solutions.component';
-import { UseCaseHeroComponent } from './usecase/use-case-hero/use-case-hero.component';
+import { OurExpertiseComponent } from './solutions/our-expertise/our-expertise.component';
+import { SolutionAffluenceHeroComponent } from './solutions/solution-affluence-hero/solution-affluence-hero.component';
+import { SolutionBlockComponent } from './solutions/solution-block/solution-block.component';
+import { SolutionCarouselComponent } from './solutions/solution-carousel/solution-carousel.component';
+import { SolutionGridComponent } from './solutions/solution-grid/solution-grid.component';
+import { WebAppFeaturesComponent } from './solutions/web-app-features/web-app-features.component';
+import { SolutionWebAppHeroComponent } from './solutions/web-app-hero/web-app-hero.component';
+import { ChallengeComponent } from './usecase/challenge/challenge.component';
 import { CustomerNeedComponent } from './usecase/customer-need/customer-need.component';
 import { ResponseComponent } from './usecase/response/response.component';
+import { UseCaseHeroComponent } from './usecase/use-case-hero/use-case-hero.component';
 import { UseCaseQuoteComponent } from './usecase/use-case-quote/use-case-quote.component';
-import { ChallengeComponent } from './usecase/challenge/challenge.component';
-import { SolutionAffluenceHeroComponent } from './solutions/solution-affluence-hero/solution-affluence-hero.component';
-import { SolutionGridComponent } from './solutions/solution-grid/solution-grid.component';
-import { TeamQuoteComponent } from './core/team-quote/team-quote.component';
 
 const impactivComponentsExports = [
   BannerComponent,
@@ -40,7 +42,6 @@ const impactivComponentsExports = [
   OurCustomersComponent,
   OurSolutionsComponent,
   OurSkillsComponent,
-  QuoteComponent,
   WebAppFeaturesComponent,
   HomeHeroComponent,
   StatsNumbersComponent,
@@ -60,6 +61,9 @@ const impactivComponentsExports = [
   SolutionGridComponent,
   SolutionAffluenceHeroComponent,
   TeamQuoteComponent,
+  CompanyHeroComponent,
+  OurPartnersComponent,
+  TimelineComponent,
 ];
 
 @NgModule({
