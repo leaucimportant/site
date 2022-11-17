9d9a90522d270f0974f9b8b393cbe1f347d62187
Amélie LAURENT <a.laurent@impactiv.fr>
2022-11-17 11:22:15 +0000

    Resolve "Screen Masonry"

--- src/app/components/solutions/affluence-masonry/affluence-masonry.component.ts
+++ src/app/components/solutions/affluence-masonry/affluence-masonry.component.ts
@@ -16,7 +16,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'Améliorez l’expérience utilisateur',
     description:
       'Fluidifiez l’affluence de votre point de vente, vecteur de satisfaction client.',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/ux.svg',
     image: 'assets/images/contents/solutions/affluence/masonry/ux.png',
     black: true,
     imagefill: false,
@@ -28,7 +28,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'Métriques consultables multi-supports',
     description:
       'Notre solution est livrée avec un back-office simple et intuitif qui vous donne un état des lieux en temps réel de l’affluence de votre point de vente ainsi qu’une API à votre disposition.',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/multi.svg',
     black: false,
   };
 
@@ -36,7 +36,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'Analysez le flux',
     description:
       'Véritable outil de performance, nos capteurs vous permettront d’avoir une meilleure connaissance de vos clients et de leur parcours. Adaptez votre offre/merchandising en conséquence !',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/analyse.svg',
     image: 'assets/images/contents/solutions/affluence/masonry/flux.png',
     black: false,
     imagefill: true,
@@ -48,7 +48,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'Graphiques temps réel',
     description:
       'Vos clients disposent d’un accès aux données d’affluence, consultable via un simple lien web ou intégré sur votre site/application native client.',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/graphique.svg',
     image: 'assets/images/contents/solutions/affluence/masonry/graphique.png',
     black: false,
     imagefill: false,
@@ -60,7 +60,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'De nombreuses applications',
     description:
       'Nos capteurs permettent non seulement de mesurer l’affluence de vos clients mais peuvent s’appliquer à d’autres cas d’usage (mesure de stocks avec réapprovisionnement automatique,…).',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/applications.svg',
     image: 'assets/images/contents/solutions/affluence/masonry/applications.png',
     black: true,
     imagefill: false,
@@ -72,7 +72,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'Limiter le nombre de visiteurs',
     description:
       'Vous pouvez, sur la base des données remontées en temps réel, limiter l’accès à votre point de vente en fonction du flux de clients.',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/limiter.svg',
     black: true,
   };
 
@@ -80,7 +80,7 @@ export class AffluenceMasonryComponent implements OnInit {
     title: 'Plug and Play',
     description:
       'Nos capteurs partent configurés pour votre environnement, vous n’avez plus qu’à les brancher aux prérequis techniques indiqués en amont.',
-    icon: 'assets/images/icons/infrastructure/infrastructure.svg',
+    icon: 'assets/images/icons/affluence/plug.svg',
     image: 'assets/images/contents/solutions/affluence/masonry/plug.png',
     black: false,
     imagefill: false,
