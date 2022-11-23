be0aa43de3be14e19b9dabb1f8c852356424a725
Elias GHAZALI <e.ghazali@impactiv.fr>
2022-11-23 09:27:46 +0000

    Resolve "Scroll navbar"

--- src/app/components/core/header/header.component.ts
+++ src/app/components/core/header/header.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { Subscription } from 'rxjs';
-import { Config, UseCaseService } from 'src/app/services';
+import { Config, HeaderService, UseCaseService } from 'src/app/services';
 import { impactivRoutes } from 'src/app/services/config';
 
 @Component({
@@ -9,7 +16,12 @@ import { impactivRoutes } from 'src/app/services/config';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-  constructor(private useCaseService: UseCaseService) {}
+  constructor(
+    private useCaseService: UseCaseService,
+    @Inject(DOCUMENT) private document: Document,
+    private window: Window,
+    private headerService: HeaderService
+  ) {}
   routes = Config.impactivRoutes;
   hoveringIndex: null | number = null;
   solutions = [
@@ -50,7 +62,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     logo?: string;
   }[] = [];
   useCasesSubscription!: Subscription;
-
+  prevScrollPos = this.window.scrollY;
   ngOnInit(): void {
     this.useCasesSubscription = this.useCaseService
       .getUseCases$()
@@ -68,5 +80,32 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.useCasesSubscription.unsubscribe();
+  }
+
+  @HostListener('window:scroll', [])
+  headerScroll() {
+    const navbar = this.document.getElementById('navbar');
+    const currentScrollPos = this.window.scrollY;
+    if (navbar) {
+      if (this.window.innerWidth >= 1140) {
+        if (this.prevScrollPos > currentScrollPos) {
+          navbar.style.top = '0';
+          navbar.classList.add('blur');
+        } else {
+          navbar.style.top = '-5.75rem';
+          navbar.classList.remove('blur');
+        }
+      } else {
+        navbar.classList.add('blur');
+      }
+      if (navbar.offsetHeight - 50 > currentScrollPos) {
+        navbar.classList.remove('blur');
+      }
+    }
+    this.prevScrollPos = currentScrollPos;
+  }
+
+  deactivateScroll() {
+    this.headerService.deactivateScroll();
   }
 }
