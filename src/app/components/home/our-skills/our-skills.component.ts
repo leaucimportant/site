306d25da690053980cd12cd6503ee34cb91817f2
Elias GHAZALI <e.ghazali@impactiv.fr>
2023-02-16 14:28:31 +0100

    feat: our skills

--- src/app/components/home/our-skills/our-skills.component.ts
+++ src/app/components/home/our-skills/our-skills.component.ts
@@ -11,37 +11,32 @@ export class OurSkillsComponent implements OnInit {
 
   skills = [
     {
-      title: 'La conception de l’expérience utilisateur.',
-      description:
-        'Nous pensons et travaillons de l’abstrait au concret. Nous aimons débuter avec la vision d’ensemble d’un nouveau projet et essayons par la suite d’en définir l’objectif principal. Tout ce que nous créons ensuite s’enligne dans cette direction et nous définissons le projet au fur et à mesure que nous avançons.',
+      title: $localize`:Text|title skill @@OUR-SKILLS-UX:`,
+      description: $localize`:Text|description skill @@OUR-SKILLS-UX-DESCRIPTION:`,
       image: 'acc-ux.png',
-      alt: 'conception de l’expérience utilisateur',
+      alt: "conception de l'expérience utilisateur",
     },
     {
-      title: 'Designer avec un objectif.',
-      description:
-        'La phase créative du projet nous permet de réfléchir au parcours utilisateur et de créer une direction artistique adéquate pour le projet. Nous établissons un design système et créons un pont entre la marque et le digital. Nous vous promettons ainsi que les aspects immersifs de votre projet digital fonctionneront parfaitement dans un contexte réel.',
+      title: $localize`:Text|title skill @@OUR-SKILLS-UI:`,
+      description: $localize`:Text|description skill @@OUR-SKILLS-UI-DESCRIPTION:`,
       image: 'acc-design.png',
       alt: 'Designer avec un objectif',
     },
     {
-      title: 'Développement influencé par la méthode Agile.',
-      description:
-        "Notre plus haute priorité est de satisfaire le client. Nous respectons les valeurs et les principes Agile et nous nous engageons à réagir rapidement aux besoins de nos clients. Notre équipe transverse accorde une attention particulière à l'excellence technique dans chaque élément. Un logiciel opérationnel est la principale mesure du succès de projet.",
+      title: $localize`:Text|title skill @@OUR-SKILLS-AGILE:`,
+      description: $localize`:Text|description skill @@OUR-SKILLS-AGILE-DESCRIPTION:`,
       image: 'acc-dev.png',
       alt: 'Développement influencé par la méthode Agile',
     },
     {
-      title: 'Infrastructure et back-end',
-      description:
-        "Le passage au cloud est un investissement judicieux pour l'avenir de votre business : vous devenez plus agile, vous pouvez adapter votre infrastructure à votre croissance, employer les technologies les plus adaptées et améliorer la sécurité. Quel que soit votre objectif, notre expertise peut vous aider à mener facilement à bien vos projets cloud.",
+      title: $localize`:Text|title skill @@OUR-SKILLS-INFRASTRUCTURE:`,
+      description: $localize`:Text|description skill @@OUR-SKILLS-INFRASTRUCTURE-DESCRIPTION:`,
       image: 'acc-infra.png',
       alt: 'Infrastructure et back-end',
     },
     {
-      title: 'Un suivi à chaque étape de votre projet.',
-      description:
-        'En intégrant l’ensemble de la valeur de votre projet en interne, nous vous accompagnons sur toute la durée de votre projet, de la co-création de votre cahier des charges au déploiement et au-delà.',
+      title: $localize`:Text|title skill @@OUR-SKILLS-FOLLOW-UP:`,
+      description: $localize`:Text|description skill @@OUR-SKILLS-FOLLOW-UP-DESCRIPTION:`,
       image: 'acc-suvi.png',
       alt: 'Un suivi à chaque étape de votre projet',
     },
--- src/environments/translations/impactiv.fr.ts
+++ src/environments/translations/impactiv.fr.ts
@@ -51,4 +51,20 @@ export const impactivFr = {
   'NUMBERS-YEARS': 'ans',
   'NUMBERS-TOTAL-CODE': 'De lignes de code',
   'NUMBERS-CODE': 'Soumissions de code sur les 3 derniers mois',
+  'OUR-SKILLS-TITLE': 'Une équipe dédiée à chacun de vos besoins.',
+  'OUR-SKILLS-UX': "La conception de l'expérience utilisateur.",
+  'OUR-SKILLS-UX-DESCRIPTION':
+    "Nous pensons et travaillons de l'abstrait au concret. Nous aimons débuter avec la vision d'ensemble d'un nouveau projet et essayons par la suite d'en définir l'objectif principal. Tout ce que nous créons ensuite s'enligne dans cette direction et nous définissons le projet au fur et à mesure que nous avançons.",
+  'OUR-SKILLS-UI': 'Designer avec un objectif.',
+  'OUR-SKILLS-UI-DESCRIPTION':
+    'La phase créative du projet nous permet de réfléchir au parcours utilisateur et de créer une direction artistique adéquate pour le projet. Nous établissons un design système et créons un pont entre la marque et le digital. Nous vous promettons ainsi que les aspects immersifs de votre projet digital fonctionneront parfaitement dans un contexte réel.',
+  'OUR-SKILLS-AGILE': 'Développement influencé par la méthode Agile.',
+  'OUR-SKILLS-AGILE-DESCRIPTION':
+    "Notre plus haute priorité est de satisfaire le client. Nous respectons les valeurs et les principes Agile et nous nous engageons à réagir rapidement aux besoins de nos clients. Notre équipe transverse accorde une attention particulière à l'excellence technique dans chaque élément. Un logiciel opérationnel est la principale mesure du succès de projet.",
+  'OUR-SKILLS-INFRASTRUCTURE': 'Infrastructure et back-end',
+  'OUR-SKILLS-INFRASTRUCTURE-DESCRIPTION':
+    "Le passage au cloud est un investissement judicieux pour l'avenir de votre business : vous devenez plus agile, vous pouvez adapter votre infrastructure à votre croissance, employer les technologies les plus adaptées et améliorer la sécurité. Quel que soit votre objectif, notre expertise peut vous aider à mener facilement à bien vos projets cloud.",
+  'OUR-SKILLS-FOLLOW-UP': 'Un suivi à chaque étape de votre projet.',
+  'OUR-SKILLS-FOLLOW-UP-DESCRIPTION':
+    "En intégrant l'ensemble de la valeur de votre projet en interne, nous vous accompagnons sur toute la durée de votre projet, de la co-création de votre cahier des charges au déploiement et au-delà.",
 };
