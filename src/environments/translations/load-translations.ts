766a882ad3cea01ea40f7a908c070a0f0618430d
leaucimportant <elias.ghazali99@gmail.com>
2023-03-10 08:20:02 +0000

    Update 2 files

--- src/environments/translations/load-translations.ts
+++ src/environments/translations/load-translations.ts
@@ -2,26 +2,30 @@ import { loadTranslations } from '@angular/localize';
 import '@angular/localize/init';
 
 import { impactivEn } from './impactiv.en';
+import { impactivEs } from './impactiv.es';
 import { impactivFr } from './impactiv.fr';
 
 export const loadLocalTranslations = () => {
   const lang = getLanguage();
+
   if (lang === 'en') {
     loadTranslations(impactivEn);
+  } else if (lang === 'es') {
+    loadTranslations(impactivEs);
   } else {
     loadTranslations(impactivFr);
   }
 };
 
-export const getLanguage: () => 'en' | 'fr' | null = () => {
+export const getLanguage: () => string | null = () => {
   const lang = localStorage.getItem('lng');
-  if (lang === 'en' || lang === 'fr') {
+  if (lang) {
     return lang;
   }
   return null;
 };
 
-export const setLanguage = (lang: 'en' | 'fr' | null) => {
+export const setLanguage = (lang: string | null) => {
   if (lang == null) {
     localStorage.removeItem('lng');
   } else {
